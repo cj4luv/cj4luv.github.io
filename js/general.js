3b0bc1de2f621455e09cb47c50e0c1e9fc1fa126
fifty bridge <cj4luv@gmail.com>
2018-08-16 00:18:18 +0900

    포트폴리오 사이트 교체

---
+++ js/general.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // alert("첫번째 ready()");
+  $(".hamburger").removeClass("hidden");
+});
+
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if(height  > 200) {
+      $(".freccia").css("visibility","hidden");
+  }
+  if(height  < 300) {
+    $('#page-title').addClass('hidden');
+  }
+  if(height  > 400) {
+    $('#page-title').removeClass('hidden');
+  }
+});
